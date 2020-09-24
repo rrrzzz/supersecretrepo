6eb4387eda083ff884c9e87a9814d52bab0b55e5
Alexey Shutov <a_sh@phygitalism.com>
2020-09-24 13:08:16 +0300

    xr support, camera placement

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
