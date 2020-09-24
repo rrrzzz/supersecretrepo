e026f52057c0048a89e984c2df579e33687357a5
Alexey Shutov <a_sh@phygitalism.com>
2020-09-24 23:35:33 +0300

    added 2015-2016 photos

---
+++ Assets/Editor/PlanePlacer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[ExecuteAlways]
+public class PlanePlacer : MonoBehaviour
+{
+    private int i;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    [MenuItem("MyTools/CreateGameObjects")]
+    void Create()
+    {
+        for (int x=0; x!=10; x++)
+        {
+            GameObject go = new GameObject($"MyCreatedGO{i++}");
+            go.transform.position = new Vector3(x,0,0);
+        }
+    }
+}
